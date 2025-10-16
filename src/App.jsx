a1b8a366d32b51dc6007d16fdb8d877e85c53c4b
dcho72801-lab <dcho72801@gmail.com>
2025-10-16 12:17:27 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>🟩 BlackDragon Shop</h1>
+      <p>Shop random acc Free Fire uy tín - Giao dịch tự động 24/7 ⚡</p>
+
+      <button>Mua Acc Ngay</button>
+    </div>
+  );
+}
+
+export default App;
